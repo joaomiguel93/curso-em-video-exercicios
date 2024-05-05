c60ce9d17d7eabb6da5146e8a75881e26ade3ec4
João Miguel <joao_miguel@hotmail.com.br>
2024-05-05 16:37:19 -0300

    Implementação da while no exercício

--- ex037_Conversor de Bases Numéricas.py
+++ ex037_Conversor de Bases Numéricas.py
@@ -5,10 +5,19 @@ print('''[1] converter para BINÁRIO
 [2] converter para OCTAL
 [3] converter para HEXADECIMAL''')
 base = int(input('Sua opção: '))
-if base == 1:
-    print(f'O número {num} convertido para BINÁRIO é {bin(base)}')
-if base == 2:
-    print(f'O número {num} convertido para OCTAL é {oct(base)}')
-if base == 3:
-    print(f'O número {num} convertido para HEXADECIMAL é {hex(base)}')
+while True:
+    if base == 1 or base == 2 or base ==3:
+        if base == 1:
+            print(f'O número {num} convertido para BINÁRIO é {bin(base)}')
+            break
+        elif base == 2:
+            print(f'O número {num} convertido para OCTAL é {oct(base)}')
+            break
+        elif base == 3:
+            print(f'O número {num} convertido para HEXADECIMAL é {hex(base)}')
+            break
+    else:
+        print('Opção inválida. Tente novamente!')
+        base = int(input('Sua opção: '))
+
 print('>> FIM DO PROGRAMA <<')
