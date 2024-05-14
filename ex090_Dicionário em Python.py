d04ce111d511f0aba68cec3a2f4820522ecf3298
João Miguel <joao_miguel@hotmail.com.br>
2024-05-13 22:09:45 -0300

    Melhoria utilizando 'for'

--- ex090_Dicionário em Python.py
+++ ex090_Dicionário em Python.py
@@ -1,6 +1,6 @@
 # Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário. No final, mostre o conteúdo da estrutura na tela.
 aluno = dict()
-aluno['nome'] = str(input('Nome: '))
+aluno['nome'] = str(input('Nome: ')).strip()
 aluno['media'] = float(input(f'Média de {aluno['nome']}: '))
 
 # Classificando a média
@@ -11,6 +11,5 @@ elif aluno['media'] >= 5:
 else:
     aluno['situacao'] = 'Reprovado'
 print('=-=' * 10)
-print(f'  - Nome é {aluno['nome']}')
-print(f'  - Média é igual {aluno['media']}')
-print(f'  - Situação é {aluno['situacao']}')
+for k, v in aluno.items():
+    print(f'  - {k} é igual a {v}.')
