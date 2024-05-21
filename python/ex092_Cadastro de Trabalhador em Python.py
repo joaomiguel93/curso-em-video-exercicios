bcb16fdc68d4ba13e9d5f3a468d6af4e36c051c5
João Miguel <joao_miguel@hotmail.com.br>
2024-05-20 21:55:32 -0300

    Melhorias

--- python/ex092_Cadastro de Trabalhador em Python.py
+++ python/ex092_Cadastro de Trabalhador em Python.py
@@ -1,17 +1,17 @@
 # Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-o (com idade) em um dicionário. Se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário. Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
 from datetime import datetime
-
-nome = str(input('Nome: '))
+dados = {}
+dados['nome'] = str(input('Nome: '))
 anoNascimento = int(input('Ano de Nascimento: '))
-ctps = int(input('Nº da CTPS (0 não tem): '))
-if ctps > 0:
-    anoContracao = int(input('Ano de Contratação: '))
-    salario = float(input('Salário: R$ '))
+dados['idade'] = datetime.now().year - anoNascimento
+dados['ctps'] = int(input('Nº da CTPS (0 não tem): '))
+if dados['ctps'] != 0:
+    dados['contratacao'] = int(input('Ano de Contratação: '))
+    dados['salario'] = float(input('Salário: R$ '))
+    dados['aposentadoria'] = dados['idade'] + ((dados['contratacao'] + 35) - datetime.now().year)
 
-    dados = {'nome': nome, 'idade': ((datetime.now().year) - anoNascimento), 'ctps': ctps, 'contração': anoContracao, 'salario': salario}
-    dados ['aposentadoria'] = ((anoContracao + 35) - anoNascimento)
-else:
-    dados = {'nome': nome, 'idade': ((datetime.now().year) - anoNascimento)}
+print('+-' * 15)
+
 
 for k, v in dados.items():
     print(f' - {k} tem o valor {v}')
