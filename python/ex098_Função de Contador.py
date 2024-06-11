261088c05cc4fd69e78c22ccf5d4ee855f27eea1
João Miguel <joao_miguel@hotmail.com.br>
2024-06-10 21:56:22 -0300

    Resolução exercícios conforme correção da aula

--- python/ex098_Função de Contador.py
+++ python/ex098_Função de Contador.py
@@ -8,23 +8,37 @@ from time import sleep
 
 
 def contador(i, f, p):
+    if p < 0:
+        p *= -1
+    if p == 0:  # Se o passo for igual a 0, considerar igual a 1
+        p = 1
+    print('=' * 30)
     print(f'Contagem de {i} até {f} de {p} em {p}')
-    for c in range(i, f, p):
-        print(f'{c} ', end=' ', flush=True)
-        sleep(0.5)
-    print('>>> FIM DO PROGRAMA <<<')
+    sleep(2.5)
 
+    if i < f:
+        cont = i
+        while cont <= f:
+            print(f'{cont} ', end='', flush=True)
+            sleep(0.5)
+            cont += p
+        print('>>> FIM DO PROGRAMA <<<')
+    else:
+        cont = i
+        while cont >= f:
+            print(f'{cont} ', end='', flush=True)
+            sleep(0.5)
+            cont -= p
+        print('>>> FIM DE PROGRAMA <<<')
 
 # Programa principal
-contador(1, 11, 1)
-contador(10, 0, -2)
-
+contador(1, 10, 1)
+contador(10, 0, 2)
 print('=' * 30)
 print('Agora é sua vez: ')
 inicio = int(input('Digite o valor inicial: '))
 fim = int(input('Digite o valor final: '))
 passo = int(input('Digite o passo: '))
 print('=' * 30)
-
 contador(inicio, fim, passo)
 print( )
