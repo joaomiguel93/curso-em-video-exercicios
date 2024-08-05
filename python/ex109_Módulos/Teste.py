c1638ca5964ae3d9b63e2ab83d2ce8088b83062e
João Miguel <joao_miguel@hotmail.com.br>
2024-08-05 20:06:10 -0300

    Commit Inicial

--- python/ex109_Módulos/Teste.py
+++ python/ex109_Módulos/Teste.py
@@ -1,4 +1,4 @@
-from ex109_Módulos import Moedas
+from ex110_Módulos import Moedas
 
 p = float(input('Digite o preço: R$ '))
 print(f'A metade de R$ {Moedas.moeda(p)} é {Moedas.metade(p, True)}.')
---
+++ python/ex110_Módulos/Teste.py
@@ -0,0 +1,4 @@
+from ex110_Módulos import Moedas
+
+p = float(input('Digite o preço: R$ '))
+Moedas.resumo(p, 20, 12)
